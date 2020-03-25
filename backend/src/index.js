f1c3d42e6cd8af122f6e1be2c60fa6a63c6f35d9
danilocecci <danilocecci@gmail.com>
2020-03-25 20:46:53 -0300

    Segunda aula do projeto

---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/**
+ * Métodos HTTP
+ * 
+ * GET: Buscar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end 
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+/**
+ * Driver: SELECT * FROM users;
+ * Query builder: usa linguagem js. table('users').select('*').where('id=1');
+ */
+
+
+
+app.listen(3333);
